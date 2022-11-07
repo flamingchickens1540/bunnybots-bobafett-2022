f5c29e654eb2a2fe970aeb058a3f5f6d01549e70
mimizh2418 <41vin2h4n9@gmail.com>
2022-11-07 14:04:05 -0800

    feat: added arcade drive

---
+++ src/main/java/org/team1540/bobafett/commands/drivetrain/ArcadeDrive.java
@@ -0,0 +1,25 @@
+package org.team1540.bobafett.commands.drivetrain;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/** Arguably more intuitive driving system than Tank Drive.
+ * Uses left stick for throttle and right stick for turning
+ */
+public class ArcadeDrive extends CommandBase {
+    private final XboxController controller;
+    private final Drivetrain drivetrain;
+
+    public ArcadeDrive(Drivetrain drivetrain, XboxController controller) {
+        this.controller = controller;
+        this.drivetrain = drivetrain;
+        addRequirements(drivetrain);
+    }
+
+    @Override
+    public void execute() {
+        double throttle = 0.5*controller.getLeftY();
+        double turn = 0.5*controller.getRightX();
+        drivetrain.setPercent(turn - throttle, turn + throttle);
+    }
+}
